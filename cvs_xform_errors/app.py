d561b5555eebef3023193141cd7282f260c7274c
Samuel Sekiwere <sekiskylink@gmail.com>
2012-03-16 14:38:20 +0300

    Avoid handling any one in Script

--- cvs_xform_errors/app.py
+++ cvs_xform_errors/app.py
@@ -1,9 +1,11 @@
 import rapidsms
 from rapidsms.apps.base import AppBase
+from script.models import ScriptProgress
+
 class App (AppBase):
     def handle (self, message):
         # We fall to this app when xform fails to match message
-        if message.connection.contact:
+        if message.connection.contact and not ScriptProgress.objects.filter(connection=message.connection).exists():
             if message.connection.contact.healthproviderbase:
                 message.respond("Thank you for your message. We have forwarded to your DHT for follow-up. If this was meant to be a weekly report, please check and resend.")
                 return True
